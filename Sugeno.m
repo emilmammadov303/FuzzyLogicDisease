2d6f9ecf712a03b40cb03f479e1e6ab33c6e23a1
Emil Mammadov <emilmammadov303@gmail.com>
2019-01-11 00:42:52 +0300

    sugeno global

--- Sugeno.m
+++ Sugeno.m
@@ -3,7 +3,7 @@ clear;
 clear all;
 
 global mu_xi;
-altsinir=0; ustsinirSene=70; ustsinirAdet=70;
+lowLimit=0; hLimitYear=70; hLimitPiece=70;
 x = 13;  y=10;
 
 a1 = 0;         b1 = 0;         c1 = 0;
@@ -13,15 +13,15 @@ a4 = 0.75;      b4 = 0.75;      c4 = 0.75;
 a5 = 1;         b5 = 1;         c5 = 1;
 
 %Bulanik Kumelerin Olusturulmasi
-    %Sene
-ucgen(altsinir,0,0,10,ustsinirSene,x);                 seneAz = mu_xi;
-CauchyEgrisi(altsinir,ustsinirSene,13,8,1.5,x);        seneOrta = mu_xi;
-CauchyEgrisi(altsinir,ustsinirSene,70,40,2.6,x);       seneCok = mu_xi;
-    %Adet
-yamuk(altsinir,0,0,5,15,ustsinirAdet,y);               adetAz = mu_xi;
-CauchyEgrisi(altsinir,ustsinirAdet,15,8,2.5,y);        adetOrta = mu_xi;
-CauchyEgrisi(altsinir,ustsinirAdet,70,33,2.5,y);       adetCok = mu_xi;
-    %Hastalik
+    %YEAR
+triangle(lowLimit,0,0,10,hLimitYear,x);                 yearL = mu_xi;
+Cauch(lowLimit,hLimitYear,13,8,1.5,x);                  yearM = mu_xi;
+Cauchy(lowLimit,hLimitYear,70,40,2.6,x);                yearH = mu_xi;
+    %PIECE
+trapezoid(lowLimit,0,0,5,15,hLimitPiece,y);             pieceL = mu_xi;
+Cauchy(lowLimit,hLimitPiece,15,8,2.5,y);                pieceM = mu_xi;
+Cauchy(lowLimit,hLimitPiece,70,33,2.5,y);               pieceH = mu_xi;
+    %DISEASE
 z1 = a1*x + b1*y + c1;
 z2 = a2*x + b2*y + c2;
 z3 = a3*x + b3*y + c3;
@@ -30,27 +30,27 @@ z5 = a5*x + b5*y + c5;
 
 
     %Kurallar
-w1 = min(seneAz,adetAz);
-w2 = min(seneAz,adetOrta);
-w3 = min(seneAz,adetCok);
+w1 = min(yearL,pieceL);
+w2 = min(yearL,pieceM);
+w3 = min(yearL,adetCok);
 
-w4 = min(seneOrta,adetAz);
-w5 = min(seneOrta,adetOrta);
-w6 = min(seneOrta,adetCok);
+w4 = min(yearM,pieceL);
+w5 = min(yearM,pieceM);
+w6 = min(yearM,adetCok);
 
-w7 = min(seneCok,adetAz);
-w8 = min(seneCok,adetOrta);
-w9 = min(seneCok,adetCok);
+w7 = min(yearH,pieceL);
+w8 = min(yearH,pieceM);
+w9 = min(yearH,adetCok);
 
 SAT = w1*z1 + w2*z2 + w3*z2 + w4*z2 + w5*z3 + w6*z3 + w7*z3 + w8*z3 + w9*z5;
 
 
 
-toplam_agirlik = w1 + w2 + w3 + w4 + w5 + w6 + w7 + w8 + w9;
-if toplam_agirlik == 0
-    'Deneme'
+totalWeight = w1 + w2 + w3 + w4 + w5 + w6 + w7 + w8 + w9;
+if totalWeight == 0
+    'TW = 0'
 end
-z = SAT/toplam_agirlik
+z = SAT/totalWeight
 
 h=[z,z,z,z,z,z,z,z,z,z,z];
 line(h,0:0.1:1,'Color','red','LineStyle','--');
